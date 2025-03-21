a3b9126a6a687310e4316d45417af782c3371f88
PlopesK <gp081009@gmail.com>
2025-03-20 23:20:01 -0300

    Nest setted

---
+++ backend/src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { exec } from 'child_process';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
+
+  const url = 'http://localhost:3000';
+  const command = process.platform === 'win32' ? `start ${url}` : `open ${url}`;
+  exec(command);
+}
+bootstrap();
